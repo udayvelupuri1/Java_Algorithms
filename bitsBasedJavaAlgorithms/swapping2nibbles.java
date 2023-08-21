3ecd92d7bed5cba2fd75a7938879349f9b0f01f1
Uday Kumar <94821907+udayvelupuri1@users.noreply.github.com>
2023-08-21 10:43:10 +0530

    Update swapping2nibbles.java

--- bitsBasedJavaAlgorithms/swapping2nibbles.java
+++ bitsBasedJavaAlgorithms/swapping2nibbles.java
@@ -4,7 +4,7 @@ public class swapping2nibbles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println((n&0X0F)<<4|(n%0XF0)>>4);
+        System.out.println((n&0X0F)<<4|(n&0XF0)>>4);
     }
     
 }
